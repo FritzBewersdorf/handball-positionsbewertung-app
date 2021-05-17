26876b36f54229bf2f70052825e3771896175640
Fritz Bewersdorf <fritz.bewersdorf@gmx.de>
2021-05-17 21:00:11 +0200

    def_algo

--- src/app/def_algo.py
+++ src/app/def_algo.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv("19_06_25_DHB_Goal.csv")
+dataset = pd.read_csv("CoCoAnDa_Daten/19_06_25_DHB_Goal.csv")
+
+print(dataset)
 
 def def_algo(dataset):
 
