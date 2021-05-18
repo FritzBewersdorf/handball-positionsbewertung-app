ef2119eb3565e2483a1debdb2b4acb4a358d5a0c
Fritz Bewersdorf <fritz.bewersdorf@gmx.de>
2021-05-18 11:43:12 +0200

    algo_Def

--- src/app/def_algo.py
+++ src/app/def_algo.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 dataset = pd.read_csv("CoCoAnDa_Daten/19_06_25_DHB_Goal.csv")
+#dataset = dataset.dropna()
+
+le = LabelEncoder()
+dataset["player_name"] = le.fit_transform(dataset["player_name"])   
+dataset.rename(columns={'player_name':'player_id'}, inplace=True)
 
 print(dataset)
 
